e99054fcb67a8a8fb2cb57696ebb3b7c9a155706
Jenovic K. <kanyindajenovic69@gmail.com>
2026-05-23 17:01:15 +0200

    Modification du footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="py-8 px-4 text-center border-t dark:border-gray-700">
       <p className="text-gray-600 dark:text-gray-400">
-        © 2025 Jenovic Kanyinda - {t.footer.rights}
+        © 2026 Jenovic Kanyinda 
       </p>
     </footer>
   )
